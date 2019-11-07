4a471dfceade0acadc840734c91404cf6766171d
112 <denismaruchin@mail.ru>
2019-11-07 15:55:30 +0300

    Task1

---
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -0,0 +1,23 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task1 {
+    static void Task1() {
+        String x;
+        String y;
+        String z;
+        x = "Хорошо идут дела";
+        y = "Изучаю Java я!";
+        z = " ";
+        System.out.println(x);
+        System.out.println(y);
+        System.out.print(x);
+        System.out.print(z);
+        System.out.println(y);
+        System.out.print(y);
+        System.out.print(z);
+        System.out.println(x);
+    }
+    public static void main(String[] args) {
+        Task1();
+    }
+}
